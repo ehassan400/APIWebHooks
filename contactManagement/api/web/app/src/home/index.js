b089bbe4a8feadb028d8273d8b03f9cfc5529e0a
Eman Hassan <ehassan@hearsaycorp.com>
2021-03-02 11:58:11 -0800

    react app

---
+++ contactManagement/api/web/app/src/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>render your stuff here</div>
+    );
+  }
+}
+
+export default Home;
