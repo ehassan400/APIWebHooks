0451ef8ab7ee7d97b927b680e0cf99b7c7eb124d
Wyatt <wyattlpaul@gmail.com>
2021-03-03 09:43:47 -0800

    backend fix

--- contactManagement/listener/listener_backend/listener_app.py
+++ contactManagement/listener/listener_backend/listener_app.py
@@ -7,22 +7,22 @@ from flask import abort
 
 app = Flask(__name__)
 
-webhookInfo = {'isTriggered': false, 'date': '', data = None}
+webhookInfo = {'isTriggered': False, 'date': '', 'data': None}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         pprint.pprint(request.json)
-        webhookInfo['isTriggered'] = True;
-        webhookInfo['date'] = date.today();
-        webhookInfo['data'] = request.json;
+        webhookInfo['isTriggered'] = True
+        webhookInfo['date'] = date.today()
+        webhookInfo['data'] = request.json
         return ('success', 200)
     else:
         abort(400)
 
 
 @app.route('/webhook', methods=['Get'])
-def webhook():
+def webhook_get():
     if request.method == 'GET':
        return jsonify(webhookInfo)
     else:
